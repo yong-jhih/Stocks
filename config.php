6bc0aed9ca4b14c393ce4ff82e849943e393bdd7
Scorpius <waityouforever77@gmail.com>
2026-04-28 17:38:18 +0800

    renew

--- config.php
+++ config.php
@@ -4,5 +4,3 @@ $db_ip   = getenv('TiDB_Host');
 $db_user = getenv('TiDB_User');
 $db_pass = getenv('TIDB_PASS');
 $db_name = getenv('TIDB_NAME');
-$finmind_token = getenv('FINMIND_TOKEN');
-$fugle_token = getenv('FUGLE_TOKEN');
--- function-tools.php
+++ function-tools.php
@@ -47,3 +47,39 @@ function writeLog($pdo, $type, $content, $result)
         error_log("Critical Error: Unable to write to system_logs. " . $e->getMessage());
     }
 }
+
+function callGeminiAI($prompt = 'say hi', $model = 'gemini-2.5-flash')
+{
+    $apiKey = getenv('GEMINI_TOKEN');
+    $apiUrl = "https://generativelanguage.googleapis.com/v1beta/models/{$model}:generateContent?key={$apiKey}";
+    $payload = [
+        "contents" => [
+            [
+                "parts" => [
+                    ["text" => $prompt]
+                ]
+            ]
+        ]
+    ];
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+    try {
+        $response = curl_exec($ch);
+        if (curl_errno($ch)) return "連線失敗: " . curl_error($ch);
+        $result = json_decode($response, true);
+        curl_close($ch);
+        if (isset($result['error'])) return "AI 診斷異常: " . $result['error']['message'];
+        if (isset($result['candidates'][0]['content']['parts'][0]['text'])) {
+            $ans = trim($result['candidates'][0]['content']['parts'][0]['text']);
+            $cleanAns = preg_replace('/[*#]/u', '', $ans);
+            return $cleanAns;
+        }
+        return "AI 無法識別內容";
+    } catch (Exception $e) {
+        return "程式執行失敗: " . $e->getMessage();
+    }
+}
