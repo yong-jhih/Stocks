dee69cbc55d305ee3f80cae05c5c53c588ad6681
Scorpius <waityouforever77@gmail.com>
2026-05-04 15:17:15 +0800

    renew

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ require_once("init.php");
 
 $start_time = microtime(true);
 $results = getComponentOf00981A_FromLocal();
-echo json_encode($results);
+echo ($results);
 $end_time = microtime(true);
 $execution_time = round($end_time - $start_time, 2);
 // writeLog($pdo, 'SelectAnalysis', '篩選分析結束,共耗時 ' . $execution_time . ' 秒', 'success');
