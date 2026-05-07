b94e3e357518251e86693f54285c144afd622dfe
Scorpius <waityouforever77@gmail.com>
2026-05-07 17:35:13 +0800

    renew

--- test.php
+++ test.php
@@ -4,13 +4,13 @@ require_once("init.php");
 $date = ['2026-05-05', '2026-05-05'];
 foreach ($date as $d) {
     if (
-        checkIfDataPublished($pdo, $date, 'stock_history', 500) &&
-        checkIfDataPublished($pdo, $date, 'stock_insti', 500) &&
-        checkIfDataPublished($pdo, $date, 'stock_margin', 500) &&
-        checkIfDataPublished($pdo, $date, 'stock_sbl_total', 500) &&
-        checkIfDataPublished($pdo, $date, 'stock_sbl_sold', 500)
+        checkIfDataPublished($pdo, $d, 'stock_history', 500) &&
+        checkIfDataPublished($pdo, $d, 'stock_insti', 500) &&
+        checkIfDataPublished($pdo, $d, 'stock_margin', 500) &&
+        checkIfDataPublished($pdo, $d, 'stock_sbl_total', 500) &&
+        checkIfDataPublished($pdo, $d, 'stock_sbl_sold', 500)
     ) {
         $results = selfSelectGenerateDailyDashboard($pdo, $d, [2449]);
-        createJsonFile($pdo, $date, 'self-select', $results);
+        createJsonFile($pdo, $d, 'self-select', $results);
     }
 }
