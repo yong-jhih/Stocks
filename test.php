71873e9829b79ce727f98dcc1306b787e19797b0
Scorpius <waityouforever77@gmail.com>
2026-05-13 11:05:26 +0800

    renew

--- test.php
+++ test.php
@@ -1,16 +1,16 @@
 <?php
 require_once("init.php");
 
-$stockList = json_decode(file_get_contents("data/" . "2026-05-12" . "_self-select.json"), true);
+$stockList = json_decode(file_get_contents("data/" . "2026-05-11" . "_self-select.json"), true);
 $allData = [
-    'date' => "2026-05-12",
+    'date' => "2026-05-11",
     'stocks' => []
 ];
 
 foreach ($stockList as $stock) {
-    $data = getStockAnalysisChart($pdo, $stock['stock_id'], "2026-05-12");
+    $data = getStockAnalysisChart($pdo, $stock['stock_id'], "2026-05-11");
     if ($data) {
         $allData['stocks'][$stock['stock_id']] = $data;
     }
 }
-createJsonFile($pdo, "2026-05-12", 'self-charts', $allData);
+createJsonFile($pdo, "2026-05-11", 'self-charts', $allData);
