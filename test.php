85d9d72cd7c9b19a52c91b58e2a1b27e79743f1e
Scorpius <waityouforever77@gmail.com>
2026-05-13 16:20:38 +0800

    renew

--- test.php
+++ test.php
@@ -2,6 +2,7 @@
 require_once("init.php");
 
 
-$targetDate='2026-05-12';
-$results = tetsGenerateDailyDashboard($pdo, $targetDate);
-createJsonFile($pdo, $targetDate, 'test', $results);
+$targetDate = '2026-05-12';
+// $results = tetsGenerateDailyDashboard($pdo, $targetDate);
+// createJsonFile($pdo, $targetDate, 'test', $results);
+renewCharts($pdo, $targetDate, 'test', 'test-charts');
