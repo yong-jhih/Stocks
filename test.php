cb9f3bd5e0b08153c8dd7c97aad54985484072fb
Scorpius <waityouforever77@gmail.com>
2026-05-19 11:13:05 +0800

    reneew

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 require_once("init.php");
 
 
-$targetDate = '2026-05-18';
+$targetDate = '2026-05-15';
 $results = testgenerateDailyDashboard($pdo, $targetDate);
 createJsonFile($pdo, $targetDate, 'test', $results);
 renewCharts($pdo, $targetDate, 'test', 'test-charts');
