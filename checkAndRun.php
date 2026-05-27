02c6ef86c65e9733b2c96dbdd4ce56ff847caa86
Scorpius <waityouforever77@gmail.com>
2026-05-27 16:04:55 +0800

    renew

--- checkAndRun.php
+++ checkAndRun.php
@@ -37,4 +37,37 @@ if (
 
     cleanData(20);
     lineNotification($pdo, getenv('LINE_TARGET'), '今日盤後篩選及評分排行已完成,請稍候佈署 - https://yong-jhih.github.io/Stocks/');
+
+
+    $gas_url = getenv('GAS_URL_TRIGGERS');
+    $data = [
+        'message'   => 'PHP 執行步驟已完成！',
+        'sender'    => 'PHP Web Server',
+        'timestamp' => date('Y-m-d H:i:s')
+    ];
+    $json_data = json_encode($data);
+    $ch = curl_init($gas_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($json_data),
+        'Connection: close'
+    ]);
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        if (curl_errno($ch) == CURLE_OPERATION_TIMEDOUT) {
+            echo 'GAS 執行完成（回應超時，但已成功觸發轉移）';
+        } else {
+            echo 'cURL 錯誤: ' . curl_error($ch);
+        }
+    } else {
+        echo 'GAS 回應: ' . $response;
+    }
+    curl_close($ch);
 }
