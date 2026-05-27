e70df3ec0a7982394b8f17401ffe919b62a84678
Scorpius <waityouforever77@gmail.com>
2026-05-27 14:09:57 +0800

    renew

--- test.php
+++ test.php
@@ -25,3 +25,30 @@ require_once("function-getData.php");
 
 // $pdo = getPDOConnection();
 // $targetDate = getLatestTradingDateWithTWSE($pdo) ?? getLatestTradingDateWithFugle($pdo);
+
+
+
+
+$gas_url = getenv('GAS_URL_TRIGGERS');
+$data = [
+    'message' => 'PHP 執行步驟已完成！',
+    'sender'  => 'PHP Web Server',
+    'timestamp' => date('Y-m-d H:i:s')
+];
+$json_data = json_encode($data);
+$ch = curl_init($gas_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($json_data)
+]);
+$response = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'cURL 錯誤: ' . curl_error($ch);
+} else {
+    echo 'GAS 回應: ' . $response;
+}
+curl_close($ch);
