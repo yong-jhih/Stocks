71635484cebb36d63e4bf2d66fc15e63c5f065d5
Scorpius <waityouforever77@gmail.com>
2026-06-11 17:43:19 +0800

    renew

---
+++ maintenance.php
@@ -0,0 +1,13 @@
+<?php
+require_once("init.php");
+
+updateStockProfile($pdo);
+dbClean($pdo, 'stock_history', 'trade_date', 200);
+dbClean($pdo, 'stock_insti', 'trade_date', 200);
+dbClean($pdo, 'stock_margin', 'trade_date', 200);
+dbClean($pdo, 'stock_sbl_total', 'trade_date', 200);
+dbClean($pdo, 'stock_sbl_sold', 'trade_date', 200);
+dbClean($pdo, '00981A_component', 'trade_date', 100);
+dbClean($pdo, 'system_logs', 'log_time', 10);
+
+updateSystemLog($pdo);
--- updateStockProfile.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once("init.php");
-updateStockProfile($pdo);
-updateSystemLog($pdo);
