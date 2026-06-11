d3d5adfb7bd432474ba158fcb9eee2ce4e1c7c46
Scorpius <waityouforever77@gmail.com>
2026-06-11 13:33:18 +0800

    renew

--- function-getData.php
+++ function-getData.php
@@ -1182,7 +1182,7 @@ function insertComponentOf00981A(PDO $pdo, string $targetDate, array $data): voi
             ]);
         }
         $pdo->commit();
-    } catch (Exception $e) {
+    } catch (Throwable $e) {
         $pdo->rollBack();
         throw new RuntimeException($targetDate . " 00981A 成分股資料新增失敗: " . $e->getMessage());
     }
@@ -1279,9 +1279,8 @@ function analyzeMultiPeriodChanges(PDO $pdo, string $targetDate): array
         $notificationStr = "00981A成分股今日變動,請稍候佈署 - https://yong-jhih.github.io/Stocks/?page=00981A_component\n" . "增持共" . count($increase) . "檔\n" . "減持共" . count($decrease) . "檔\n" . "無變動共" . count($constant) . "檔\n";
         if (count($eliminate) > 0) $notificationStr .= "剔除共" . count($eliminate) . "檔:" . implode(',', $eliminate) . "\n";
         if (count($new) > 0) $notificationStr .= "新納入共" . count($new) . "檔:" . implode(',', $new) . "\n";
-        lineNotification($pdo, getenv('LINE_TARGET'), $notificationStr);
-        return $finalData;
-    } catch (PDOException $e) {
+        return [$finalData, $notificationStr];
+    } catch (Throwable $e) {
         throw new RuntimeException("Database Error: " . $e->getMessage());
     }
 }
--- update00981A.php
+++ update00981A.php
@@ -11,11 +11,13 @@ writeLog($pdo, 'update00981A', '取得交易日期 ' . $targetDate . ' 開始更
 try {
     $results = getComponentOf00981A_FromLocal($pdo, $targetDate);
     insertComponentOf00981A($pdo, $targetDate, $results);
-    $analysis = analyzeMultiPeriodChanges($pdo, $targetDate);
+    $analysis = analyzeMultiPeriodChanges($pdo, $targetDate)[0];
+    $lineNotifyStr = analyzeMultiPeriodChanges($pdo, $targetDate)[1];
     createJsonFile($pdo, $targetDate, 'componentOf00981A', $analysis, 'data');
     $end_time = microtime(true);
     $execution_time = round($end_time - $start_time, 2);
     writeLog($pdo, 'update00981A', $targetDate . ' 00981A 成分股資料 更新完成,共耗時 ' . $execution_time . ' 秒', 'end');
+    lineNotification($pdo, getenv('LINE_TARGET'), $lineNotifyStr);
     updateSystemLog($pdo);
 } catch (Throwable $e) {
     writeLog($pdo, 'update00981A', $e->getMessage(), 'error');
