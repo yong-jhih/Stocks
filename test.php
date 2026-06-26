34e2e3ec7ea973676cdcb92b3f121e006101f05a
Scorpius <waityouforever77@gmail.com>
2026-06-26 13:52:48 +0800

    renew

--- test.php
+++ test.php
@@ -2,13 +2,12 @@
 
 require_once("init.php");
 
-$stocksTSE = getStockProfileTSE($pdo);
-// $stocksTPEx = getStockProfileTPEx($pdo);
-// $stocksESM = getStockProfileESM($pdo);
-
-
-
-// $industry = json_decode(file_get_contents('data/industry_code.json'), true);
-// $industry = file_get_contents('data/industry_code.json');
-// echo json_encode($industry['2330']);
-// echo $industry['01'];
+try {
+    $stocksTSE = getStockProfileTSE($pdo);
+    $stocksTPEx = getStockProfileTPEx($pdo);
+    $stocksESM = getStockProfileESM($pdo);
+    $stocks = [...$stocksTSE, ...$stocksTPEx, ...$stocksESM];
+    updateIndustry($pdo, $stocks);
+} catch (Throwable $e) {
+    throw $e;
+}
