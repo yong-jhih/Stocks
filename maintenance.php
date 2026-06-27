f62d5a0ef339511d83d110762272057d31c3ef84
Scorpius <waityouforever77@gmail.com>
2026-06-27 13:12:58 +0800

    renew

--- maintenance.php
+++ maintenance.php
@@ -2,6 +2,7 @@
 require_once("init.php");
 
 updateStockProfile($pdo);
+writeLog($pdo, 'dbClean', '開始進行資料庫維護', 'start');
 dbClean($pdo, 'stock_history', 'trade_date', 100);
 dbClean($pdo, 'stock_insti', 'trade_date', 100);
 dbClean($pdo, 'stock_margin', 'trade_date', 100);
@@ -9,5 +10,6 @@ dbClean($pdo, 'stock_sbl_total', 'trade_date', 100);
 dbClean($pdo, 'stock_sbl_sold', 'trade_date', 100);
 dbClean($pdo, '00981A_component', 'trade_date', 100);
 dbClean($pdo, 'system_logs', 'log_time', 10);
+writeLog($pdo, 'dbClean', '資料庫維護完成', 'end');
 
 updateSystemLog($pdo);
