29ea72054470ce78b1f132be6621937f2fc1415d
Scorpius <waityouforever77@gmail.com>
2026-07-01 13:48:43 +0800

    renew

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once("init.php");
-
+$targetDate = '2026-06-30';
 $stockIds = [];
-$a = json_decode(file_get_contents('data/2026-06-30_componentOf00981A.json.json'), true);
+$a = json_decode(file_get_contents("data/{$targetDate}_componentOf00981A.json"), true);
 foreach ($a as $v) {
     $stockIds[] = $v['stock_id'];
 }
