ada19d8eef35f5b714bf0437dcd44c1562e61069
Scorpius <waityouforever77@gmail.com>
2026-07-20 10:57:56 +0800

    renew

--- test.php
+++ test.php
@@ -1,11 +1,15 @@
 <?php
 require_once("init.php");
-$targetDate = '2026-07-09';
 
+// updateStockProfile($pdo);
+writeLog($pdo, 'dbClean', '開始進行資料庫維護', 'start');
+dbClean($pdo, 'stock_history', 'trade_date', 110);
+dbClean($pdo, 'stock_insti', 'trade_date', 110);
+dbClean($pdo, 'stock_margin', 'trade_date', 110);
+dbClean($pdo, 'stock_sbl_total', 'trade_date', 110);
+dbClean($pdo, 'stock_sbl_sold', 'trade_date', 110);
+dbClean($pdo, 'etf_component', 'trade_date', 110);
+dbClean($pdo, 'system_logs', 'log_time', 10);
+writeLog($pdo, 'dbClean', '資料庫維護完成', 'end');
 
-
-
-$table = ['stock_history', 'stock_insti', 'stock_margin', 'stock_sbl_total', 'stock_sbl_sold'];
-$results = generateDailyDashboard($pdo, $targetDate, $table);
-createJsonFile($pdo, $targetDate . '_test', $results);
-renewCharts($pdo, $targetDate, 'test', 'charts');
+updateSystemLog($pdo);
