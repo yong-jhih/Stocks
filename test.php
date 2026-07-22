e1c70e89368a8e7a3a861c82e97b3554bb9910ed
yong-jhih <47603841+yong-jhih@users.noreply.github.com>
2026-07-22 21:28:14 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,19 +1,3 @@
 <?php
 require_once("init.php");
-$targetDate = '2026-07-21';
-$etfId = '00981A';
-
-try {
-    $analyzeMultiPeriodChanges = analyzeMultiPeriodChanges($pdo, $targetDate, '00981A');
-    $analysis = $analyzeMultiPeriodChanges[0];
-    $stockIds = [];
-    $a = json_decode(file_get_contents("data/{$targetDate}_componentOf00981A.json"), true);
-    foreach ($a as $v) {
-        $stockIds[] = $v['stock_id'];
-    }
-
-    $result = getEtfComponentChartData($pdo,  '00981A',  $targetDate, $stockIds);
-    createJsonFile($pdo, $targetDate . '_00981A-charts_test', $result);
-} catch (Throwable $e) {
-    exit(1);
-}
+        lineNotification($pdo, getenv('LINE_TARGET'),'今日盤後篩選及評分排行已完成, 請稍候佈署 - https://yong-jhih.github.io/Stocks/');
