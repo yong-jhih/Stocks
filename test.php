059c06dd3aeba1cdda66a0f6e8697d48a162ef30
yong-jhih <47603841+yong-jhih@users.noreply.github.com>
2026-07-23 23:13:18 +0800

    Update test.php

--- test.php
+++ test.php
@@ -33,17 +33,11 @@ if ( // 已公布 檢查資料量 足夠 直接進行分析
     checkIfDataPublished($pdo, $targetDate, 'TPEx_stock_sbl_total', 500) &&
     checkIfDataPublished($pdo, $targetDate, 'TPEx_stock_sbl_sold', 500)
 ) {
-    $start_time = microtime(true);
-    $log = testRetry($pdo);
-    if (!($log['log_type'] === 'generateDailyDashboard' && ($log['result'] === 'start' || $log['result'] === 'retry'))) {
-      //  writeLog($pdo, 'generateDailyDashboard', "[{$targetDate}] 資料數量正常, 開始進行盤後篩選及評分排行", 'start');
-    }
+    echo '分析開始'；
 
     try {
         $table = ['stock_history', 'stock_insti', 'stock_margin', 'stock_sbl_total', 'stock_sbl_sold'];
-        $results = generateDailyDashboard($pdo, $targetDate, $table);
-        createJsonFile($pdo, $targetDate . '_filter', $results);
-        renewCharts($pdo, $targetDate, 'filter', 'charts');
+        
 
         $resultsTop = topPerformingGenerateDailyDashboard($pdo, $targetDate, $table);
         createJsonFile($pdo, $targetDate . '_topPerforming', $resultsTop);
@@ -67,10 +61,4 @@ if ( // 已公布 檢查資料量 足夠 直接進行分析
             exit(1);
         }
     }
-} else { // 已公布 資料量不足 則更新資料
-    writeLog($pdo, 'updateAllHistory', "偵測 [{$targetDate}] TWT93U 信用額度總量管制餘額已公布, 準備進行更新歷史資料", 'waitting');
-    updateAllHistory($pdo, $targetDate);
-    updateAllTPExHistory($pdo, $targetDate);
-    writeLog($pdo, 'updateAllHistory', '歷史資料更新完畢, 等待下階段進入分析', 'waitting');
-    updateSystemLog($pdo);
-       }
+}
