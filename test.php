ee037d39e996797878c185d4f7394bb88ae8efe5
Scorpius <waityouforever77@gmail.com>
2026-07-23 16:05:35 +0800

    renew

--- test.php
+++ test.php
@@ -1,3 +1,21 @@
 <?php
 require_once("init.php");
-        lineNotification($pdo, getenv('LINE_TARGET'),'今日盤後篩選及評分排行已完成, 請稍候佈署 - https://yong-jhih.github.io/Stocks/');
+$targetDate = '2026-07-22';
+
+
+$tableTWSE = ['stock_history', 'stock_insti', 'stock_margin', 'stock_sbl_total', 'stock_sbl_sold'];
+$resultsTWSE = topPerformingGenerateDailyDashboard($pdo, $targetDate, $tableTWSE);
+
+$tableTPEx = ['TPEx_stock_history', 'TPEx_stock_insti', 'TPEx_stock_margin', 'TPEx_stock_sbl_total', 'TPEx_stock_sbl_sold'];
+$resultsTPEx = topPerformingGenerateDailyDashboard($pdo, $targetDate, $tableTPEx);
+// createJsonFile($pdo, $targetDate . '_filter', $results);
+// renewCharts($pdo, $targetDate, 'filter', 'charts');
+$a = array_merge($resultsTWSE, $resultsTPEx);
+
+echo "TWSE:" . count($resultsTWSE) . "\n";
+echo "TPEx:" . count($resultsTPEx) . "\n";
+echo "Mix:" . count($a) . "\n";
+
+// $resultsTop = topPerformingGenerateDailyDashboard($pdo, $targetDate, $table);
+createJsonFile($pdo, $targetDate . 'test', $a);
+// renewCharts($pdo, $targetDate, 'topPerforming', 'topPerforming-charts');
