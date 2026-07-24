4939a70f9921cba2f55eab67387915dc30112299
Scorpius <waityouforever77@gmail.com>
2026-07-24 14:18:05 +0800

    renew

---
+++ fetch_00991A.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+(async () => {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const dd = String(today.getDate()).padStart(2, '0');
+    const date = `${yyyy}/${mm}/${dd}`;
+    // const url = `https://www.fhtrust.com.tw/api/assets?fundID=ETF23&qDate=${date}`;
+    const url = `https://www.fhtrust.com.tw/api/assets?fundID=ETF23&qDate=2026/07/23`;
+    console.log(url);
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`HTTP ${res.status}`);
+    }
+    const json = await res.json();
+    fs.writeFileSync('etf_componet_00991A.json', JSON.stringify(json, null, 2), 'utf8');
+    console.log('完成');
+})();
