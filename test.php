9ce9af170262bbb4aaa9cd7865b8b28f7ddcdd32
Scorpius <waityouforever77@gmail.com>
2026-07-31 13:31:23 +0800

    renew

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 require_once("init.php");
-$targetDate = '2026-07-29';
+$targetDate = '2026-07-31';
 
 // $tableTWSE = ['stock_history', 'stock_insti', 'stock_margin', 'stock_sbl_total', 'stock_sbl_sold'];
 // $resultsTWSE = topPerformingGenerateDailyDashboard($pdo, $targetDate, $tableTWSE);
