323ab76ab1b9ae8db483604a00091f7d2fead9e0
Scorpius <waityouforever77@gmail.com>
2026-08-06 09:57:18 +0800

    renew

--- test.php
+++ test.php
@@ -1,14 +1,15 @@
 <?php
- require_once("init.php");
+require_once("init.php");
+$targetDate = '2026-08-05';
 
 $tableTWSE = ['stock_history', 'stock_insti', 'stock_margin', 'stock_sbl_total', 'stock_sbl_sold'];
-        $tableTPEx = ['TPEx_stock_history', 'TPEx_stock_insti', 'TPEx_stock_margin', 'TPEx_stock_sbl_total', 'TPEx_stock_sbl_sold'];
-        $resultsTWSE = generateDailyDashboard($pdo, $targetDate, $tableTWSE);
-        $resultsTPEx = generateDailyDashboard($pdo, $targetDate, $tableTPEx);
-        // $resultsTopTWSE = topPerformingGenerateDailyDashboard($pdo, $targetDate, $tableTWSE);
-        // $resultsTopTPEx = topPerformingGenerateDailyDashboard($pdo, $targetDate, $tableTPEx);
-echo "TSE:".count($resultsTWSE)."\n";
-echo "TPEx:".count($resultsTPEx)."\n";
+$tableTPEx = ['TPEx_stock_history', 'TPEx_stock_insti', 'TPEx_stock_margin', 'TPEx_stock_sbl_total', 'TPEx_stock_sbl_sold'];
+$resultsTWSE = generateDailyDashboard($pdo, $targetDate, $tableTWSE);
+$resultsTPEx = generateDailyDashboard($pdo, $targetDate, $tableTPEx);
+// $resultsTopTWSE = topPerformingGenerateDailyDashboard($pdo, $targetDate, $tableTWSE);
+// $resultsTopTPEx = topPerformingGenerateDailyDashboard($pdo, $targetDate, $tableTPEx);
+echo "TSE:" . count($resultsTWSE) . "\n";
+echo "TPEx:" . count($resultsTPEx) . "\n";
         // $resultsMix = [...$resultsTWSE, ...$resultsTPEx];
        // createJsonFile($pdo, $targetDate . '_filter', $resultsMix);
      //   renewCharts($pdo, $targetDate, 'filter', 'charts');
