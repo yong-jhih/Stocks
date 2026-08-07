c109260e75e2df2186726bc58f5eb90fbbffbb08
Scorpius <waityouforever77@gmail.com>
2026-08-07 12:38:11 +0800

    renew

--- test.php
+++ test.php
@@ -2,5 +2,5 @@
 require_once("init.php");
 
 $targetDate = "2026-08-06";
-$a = getDataWithFinmind($pdo, $targetDate, $targetDate, "");
+$a = getDataWithFinmind($pdo, $targetDate, $targetDate, "TaiwanFuturesInstitutionalInvestors");
 echo json_encode($a);
