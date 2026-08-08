fcf9ef2e014124fcbd619080bbd15f4068d39fd1
Scorpius <waityouforever77@gmail.com>
2026-08-08 11:20:14 +0800

    renew

--- test.php
+++ test.php
@@ -5,9 +5,9 @@ $targetDate = "2026-08-07";
 try {
     $url = "https://openapi.twse.com.tw/v1/exchangeReport/FMTQIK";
     $data = fetchUrl($url);
-    if (convertTaiwanDateToWestern(end($data)['Date']) === str_replace("-", "", $targetDate)) {
-        echo json_encode(end($data));
-    }
+    // if (convertTaiwanDateToWestern(end($data)['Date']) === str_replace("-", "", $targetDate)) {
+    echo json_encode(end($data));
+    // }
     // $params = [
     //     'dataset' => "TaiwanFuturesInstitutionalInvestors",
     //     'data_id' => "TX",
