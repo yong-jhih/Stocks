46c912e625a05cd8b52cd4bfa570b9db456ac051
Scorpius <waityouforever77@gmail.com>
2026-08-08 11:14:53 +0800

    renew

--- test.php
+++ test.php
@@ -5,7 +5,9 @@ $targetDate = "2026-08-07";
 try {
     $url = "https://openapi.twse.com.tw/v1/exchangeReport/FMTQIK";
     $data = fetchUrl($url);
-
+    if (convertTaiwanDateToWestern(end($data['Date'])) === str_replace("-", "", $targetDate)) {
+        echo json_encode(end($data));
+    }
     // $params = [
     //     'dataset' => "TaiwanFuturesInstitutionalInvestors",
     //     'data_id' => "TX",
@@ -14,7 +16,7 @@ try {
     // ];
     // $a = getDataWithFinmind($pdo, $params);
     // $b = getTAIEX($targetDate);
-    echo json_encode($data);
+
 } catch (Throwable $e) {
     // callGAS([
     //     'date' => $targetDate,
