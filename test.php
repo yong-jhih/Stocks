40f010a5f48a2e9a1d279252c87c60c6430e58ee
Scorpius <waityouforever77@gmail.com>
2026-08-11 14:20:21 +0800

    test

--- test.php
+++ test.php
@@ -3,26 +3,26 @@ require_once("init.php");
 $targetDate = "2026-08-10";
 
 
-$taiex = getDataWithFinmind($pdo, [
+$taiex = end(getDataWithFinmind($pdo, [
     'dataset' => "TaiwanVariousIndicators5Seconds",
     'start_date' => $targetDate,
-])['data'];
+])['data']);
 
-$a = '';
-$url = "https://openapi.twse.com.tw/v1/exchangeReport/FMTQIK";
-for ($i = 0; $i < 3; $i++) {
-    $response = fetchUrl($url);
-    if (isset($response['stat']) && $response['stat'] === 'error') {
-        continue;
-    } else {
-        foreach ($response as $v) {
-            if (convertTaiwanDateToWestern($v['Date']) === str_replace("-", "", $targetDate)) {
-                $a = $v['TAIEX'];
-            }
-        }
-    }
-}
+// $a = '';
+// $url = "https://openapi.twse.com.tw/v1/exchangeReport/FMTQIK";
+// for ($i = 0; $i < 3; $i++) {
+//     $response = fetchUrl($url);
+//     if (isset($response['stat']) && $response['stat'] === 'error') {
+//         continue;
+//     } else {
+//         foreach ($response as $v) {
+//             if (convertTaiwanDateToWestern($v['Date']) === str_replace("-", "", $targetDate)) {
+//                 $a = $v['TAIEX'];
+//             }
+//         }
+//     }
+// }
 
 
-echo json_encode($a) . "\n";
+// echo json_encode($a) . "\n";
 echo json_encode($taiex);
