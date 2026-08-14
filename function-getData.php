0ee4aba3584e40b3af8ccb5dc073a72338e41b6e
yong-jhih <47603841+yong-jhih@users.noreply.github.com>
2026-08-14 19:43:49 +0800

    Update function-getData.php

--- function-getData.php
+++ function-getData.php
@@ -1892,6 +1892,7 @@ function updateMarketDailyData(PDO $pdo, string $targetDate): void
     $instiBuySell = getInstiBuySell($pdo, $targetDate);
     try {
         $pdo->beginTransaction();
+
         $sql = "INSERT INTO market_daily (
                     trade_date, twii_close,
                     txf_foreign_long, txf_foreign_short, txf_foreign_net,
@@ -1903,30 +1904,30 @@ function updateMarketDailyData(PDO $pdo, string $targetDate): void
                     insti_trust_buy, insti_trust_sell,
                     insti_dealer_buy, insti_dealer_sell,
                     insti_dealer_risk_buy, insti_dealer_risk_sell
-                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) AS new
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                 ON DUPLICATE KEY UPDATE
-                    twii_close = new.twii_close,
-                    txf_foreign_long = new.txf_foreign_long,
-                    txf_foreign_short = new.txf_foreign_short,
-                    txf_foreign_net = new.txf_foreign_net,
-                    mxf_foreign_long = new.mxf_foreign_long,
-                    mxf_foreign_short = new.mxf_foreign_short,
-                    mxf_foreign_net = new.mxf_foreign_net,
-                    tmf_foreign_long = new.tmf_foreign_long,
-                    tmf_foreign_short = new.tmf_foreign_short,
-                    tmf_foreign_net = new.tmf_foreign_net,
-                    mxf_retail_ratio = new.mxf_retail_ratio,
-                    txo_put_call_ratio = new.txo_put_call_ratio,
-                    insti_total_buy = new.insti_total_buy,
-                    insti_total_sell = new.insti_total_sell,
-                    insti_foreign_buy = new.insti_foreign_buy,
-                    insti_foreign_sell = new.insti_foreign_sell,
-                    insti_trust_buy = new.insti_trust_buy,
-                    insti_trust_sell = new.insti_trust_sell,
-                    insti_dealer_buy = new.insti_dealer_buy,
-                    insti_dealer_sell = new.insti_dealer_sell,
-                    insti_dealer_risk_buy = new.insti_dealer_risk_buy,
-                    insti_dealer_risk_sell = new.insti_dealer_risk_sell";
+                    twii_close = VALUES(twii_close),
+                    txf_foreign_long = VALUES(txf_foreign_long),
+                    txf_foreign_short = VALUES(txf_foreign_short),
+                    txf_foreign_net = VALUES(txf_foreign_net),
+                    mxf_foreign_long = VALUES(mxf_foreign_long),
+                    mxf_foreign_short = VALUES(mxf_foreign_short),
+                    mxf_foreign_net = VALUES(mxf_foreign_net),
+                    tmf_foreign_long = VALUES(tmf_foreign_long),
+                    tmf_foreign_short = VALUES(tmf_foreign_short),
+                    tmf_foreign_net = VALUES(tmf_foreign_net),
+                    mxf_retail_ratio = VALUES(mxf_retail_ratio),
+                    txo_put_call_ratio = VALUES(txo_put_call_ratio),
+                    insti_total_buy = VALUES(insti_total_buy),
+                    insti_total_sell = VALUES(insti_total_sell),
+                    insti_foreign_buy = VALUES(insti_foreign_buy),
+                    insti_foreign_sell = VALUES(insti_foreign_sell),
+                    insti_trust_buy = VALUES(insti_trust_buy),
+                    insti_trust_sell = VALUES(insti_trust_sell),
+                    insti_dealer_buy = VALUES(insti_dealer_buy),
+                    insti_dealer_sell = VALUES(insti_dealer_sell),
+                    insti_dealer_risk_buy = VALUES(insti_dealer_risk_buy),
+                    insti_dealer_risk_sell = VALUES(insti_dealer_risk_sell)";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([
             $targetDate,
@@ -1953,6 +1954,7 @@ function updateMarketDailyData(PDO $pdo, string $targetDate): void
             $instiBuySell['insti_dealer_risk_buy'] ?? null,
             $instiBuySell['insti_dealer_risk_sell'] ?? null,
         ]);
+
         $pdo->commit();
     } catch (Throwable $e) {
         if ($pdo->inTransaction()) {
@@ -1961,6 +1963,8 @@ function updateMarketDailyData(PDO $pdo, string $targetDate): void
         throw new RuntimeException("{$targetDate} 大盤資料新增失敗: " . $e->getMessage(), 0, $e);
     }
 }
+
+
 
 function analyzeMarketTrend(PDO $pdo): void
 {
