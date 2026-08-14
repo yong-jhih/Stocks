cd7a57474f04c2196ccda07dbc0157df73428efc
Scorpius <waityouforever77@gmail.com>
2026-08-14 17:41:35 +0800

    renew

--- test.php
+++ test.php
@@ -9,12 +9,13 @@ $date = str_replace("-", "", $targetDate);
 $url = "https://www.twse.com.tw/rwd/zh/fund/TWT43U?date={$date}&response=json";
 $data = fetchUrl($url);
 if (isset($data['stat']) && $data['stat'] === 'OK') {
-    foreach ($data['data'] as $stock) {
-        if (isset($stocksMap[$stock[0]]) && in_array($stocksMap[$stock[0]]['stock_type'], ['TSE', 'TPEx'])) {
-            $stocks[] = $stock;
-        }
-    }
-    echo count($stocks);
+    echo count($data['data']);
+    // foreach ($data['data'] as $stock) {
+    //     if (isset($stocksMap[$stock[0]]) && in_array($stocksMap[$stock[0]]['stock_type'], ['TSE', 'TPEx'])) {
+    //         $stocks[] = $stock;
+    //     }
+    // }
+    // echo count($stocks);
 } else {
     echo "getLatestTradingDateWithTWSE 證交所回傳錯誤訊息：" . ($data['stat'] ?? '未知錯誤') . "\n";
     return null;
