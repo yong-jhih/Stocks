7bfc44766ff037cd7ee46b8429089ae2a0a11de9
Scorpius <waityouforever77@gmail.com>
2026-08-17 17:40:15 +0800

    renew

--- test.php
+++ test.php
@@ -1,17 +1,14 @@
 <?php
-require_once("init.php");
-// $targetDate = '2026-08-17';
+// require_once("init.php");
+$targetDate = '2026-08-17';
 // updateMarketDailyData($pdo, $targetDate);
 
-
-// $a = json_decode(file_get_contents("pc_ratio.json"), true);
-// foreach ($a as $item) {
-//     if ($item['date'] === str_replace("-", "/", $targetDate)) {
-//         echo $item['oi_pcr'];
-//         break;
-//     }
-// }
-
-
-$a = getPutCallRatio($pdo, $targetDate);
-echo "PutCallRatio:{$a}";
+$pc = 0;
+$a = json_decode(file_get_contents("pc_ratio.json"), true);
+foreach ($a as $item) {
+    if (strtotime($item['date']) === strtotime($targetDate)) {
+        $pc = $item['oi_pcr'];
+        break;
+    }
+}
+echo $pc;
