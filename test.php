1d8127de76f4f2d73edc79502336afabce968b55
Scorpius <waityouforever77@gmail.com>
2026-08-17 17:20:48 +0800

    renew

--- test.php
+++ test.php
@@ -1,4 +1,13 @@
 <?php
 require_once("init.php");
 // $targetDate = '2026-08-17';
-updateMarketDailyData($pdo, $targetDate);
+// updateMarketDailyData($pdo, $targetDate);
+
+
+$a = json_decode(file_get_contents("pc_ratio.json"), true);
+foreach ($a as $item) {
+    if ($item['date'] === str_replace("-", "/", $targetDate)) {
+        echo $item['oi_pcr'];
+        break;
+    }
+}
