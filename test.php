453cdd529a486d954f1d3110b061da843533a62e
Scorpius <waityouforever77@gmail.com>
2026-08-28 15:29:55 +0800

    renew

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ $stock_id = "2449";
 $arg1 = escapeshellarg($stock_id);
 
 // 執行 node 腳本並取得 console.log 的輸出
-$rawOutput = shell_exec('node prefetch_TDCC.js {$arg1}');
+$rawOutput = shell_exec("node prefetch_TDCC.js {$arg1}");
 
 echo $rawOutput;
 // 解析 JSON
