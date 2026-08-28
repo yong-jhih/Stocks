987b3c5bc024a32ad21d47369ec0b67ac03d9b01
Scorpius <waityouforever77@gmail.com>
2026-08-28 15:56:12 +0800

    renew

--- test.php
+++ test.php
@@ -51,28 +51,17 @@ $date_array = [
     '20250926',
     '20250919',
     '20250912',
-    '20250905',
-    '20250829'
+    '20250905'
 ];
 
-$targetDate = '2026-08-14';
+$data = [];
 $stock_id = "2449";
-$date = str_replace("-", "", $targetDate);
-$arg1 = escapeshellarg($stock_id);
-$arg2 = escapeshellarg($date);
-
-// 執行 node 腳本並取得 console.log 的輸出
-$rawOutput = shell_exec("node prefetch_TDCC.js {$arg1} {$arg2}");
-// echo $rawOutput;
-// 解析 JSON
-$data = json_decode($rawOutput, true);
-
-if ($data && $data['status'] === 'success') {
-    echo "成功接收到 " . count($data['items']) . " 筆資料！\n";
-    foreach ($data['items'] as $item) {
-        echo "- ID: {$item['id']}, Name: {$item['name']}\n";
-    }
-} else {
-    echo "資料擷取失敗\n";
-    exit(1);
+foreach ($date_array as $date) {
+    $arg1 = escapeshellarg($stock_id);
+    $arg2 = escapeshellarg($date);
+    $rawOutput = shell_exec("node prefetch_TDCC.js {$arg1} {$arg2}");
+    $data[] = json_decode($rawOutput, true);
+    if ($date === '20260731') break;
 }
+
+echo json_encode($data);
