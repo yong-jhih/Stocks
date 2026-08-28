cd2ad94c843802ac86bf32f5d65be61288efb257
Scorpius <waityouforever77@gmail.com>
2026-08-28 15:50:51 +0800

    renew

--- test.php
+++ test.php
@@ -63,16 +63,16 @@ $arg2 = escapeshellarg($date);
 
 // 執行 node 腳本並取得 console.log 的輸出
 $rawOutput = shell_exec("node prefetch_TDCC.js {$arg1} {$arg2}");
-echo $rawOutput;
+// echo $rawOutput;
 // 解析 JSON
-// $data = json_decode($rawOutput, true);
+$data = json_decode($rawOutput, true);
 
-// if ($data && $data['status'] === 'success') {
-//     echo "成功接收到 " . count($data['items']) . " 筆資料！\n";
-//     foreach ($data['items'] as $item) {
-//         echo "- ID: {$item['id']}, Name: {$item['name']}\n";
-//     }
-// } else {
-//     echo "資料擷取失敗\n";
-//     exit(1);
-// }
+if ($data && $data['status'] === 'success') {
+    echo "成功接收到 " . count($data['items']) . " 筆資料！\n";
+    foreach ($data['items'] as $item) {
+        echo "- ID: {$item['id']}, Name: {$item['name']}\n";
+    }
+} else {
+    echo "資料擷取失敗\n";
+    exit(1);
+}
