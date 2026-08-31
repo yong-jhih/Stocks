cb60fe80b2dfabfa679433a38d710d7a44cb5271
Scorpius <waityouforever77@gmail.com>
2026-08-31 11:57:46 +0800

    renew

--- test.php
+++ test.php
@@ -8,29 +8,16 @@ $stocks = [
     'targetDate' => str_replace("-", "", $targetDate),
     'stock_id_array' => []
 ];
-$i = 0;
 foreach ($stocksMap as $stock_id => $stock) {
     if (in_array($stock['stock_type'], ["TSE", "TPEx"])) {
         $stocks['stock_id_array'][] = $stock_id;
-        $i++;
     }
-    if ($i > 5) break;
 }
 $taskFile = 'tdcc_task.json';
-file_put_contents(
-    $taskFile,
-    json_encode(
-        $stocks,
-        JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT
-    )
-);
-$output = shell_exec(
-    "node prefetch_TDCC_batch.js " .
-        escapeshellarg($taskFile)
-);
+file_put_contents($taskFile, json_encode($stocks, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+$output = shell_exec("node prefetch_TDCC_batch.js " . escapeshellarg($taskFile));
 $results = json_decode($output, true);
 $count = count($results);
-
 $sql = "
     INSERT INTO stock_shareholder
     (
@@ -60,11 +47,9 @@ try {
     $pdo->commit();
 } catch (Throwable $e) {
     if ($pdo->inTransaction()) $pdo->rollBack();
-    throw $e;
+    writeLog($pdo, 'updateTDCC', $e->getMessage(), 'error');
+    exit(1);
 }
-
-// echo "================ 結果 ================\n";
-// print_r($results);
 $end_time = microtime(true);
 $execution_time = round($end_time - $start_time, 2);
 echo "\n執行時間: {$execution_time} 秒\n";
