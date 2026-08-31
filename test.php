6afb60e11fcf398c1fe589cf36c83c9cc2500c15
Scorpius <waityouforever77@gmail.com>
2026-08-31 10:00:59 +0800

    test

--- test.php
+++ test.php
@@ -5,38 +5,24 @@ require_once("init.php");
 $targetDate = '2026-08-21';
 $stocksMap = getStocksMap();
 $stocks = [
-    'targetDate' => $targetDate,
+    'targetDate' => escapeshellarg(str_replace("-", "", $targetDate)),
     'stock_id_array' => []
 ];
+$i = 0;
 foreach ($stocksMap as $stock_id => $stock) {
     if (in_array($stock['stock_type'], ["TSE", "TPEx"])) {
         $stocks['stock_id_array'][] = $stock_id;
-        // $arg1 = escapeshellarg($stock_id);
-        // $arg2 = escapeshellarg(str_replace("-", "", $targetDate));
-        // $rawOutput = json_decode(shell_exec("node prefetch_TDCC.js {$arg1} {$arg2}"));
-        // try {
-        //     $pdo->beginTransaction();
-        //     $sql = "INSERT INTO stock_shareholder
-        //         (trade_date, stock_id, shareholder_count, total_shares)
-        //         VALUES (?, ?, ?, ?)
-        //         ON DUPLICATE KEY UPDATE
-        //         shareholder_count = VALUES(shareholder_count),
-        //         total_shares = VALUES(total_shares)";
-        //     $stmt = $pdo->prepare($sql);
-        //     $stmt->execute([
-        //         $targetDate,
-        //         $stock_id,
-        //         (int)str_replace(",", "", $rawOutput[2]),
-        //         (int)str_replace(",", "", $rawOutput[3])
-        //     ]);
-        //     $pdo->commit();
-        // } catch (Throwable $e) {
-        //     $pdo->rollBack();
-        // }
+        $i++;
     }
+    if ($i > 1) break;
 }
-$count = count($stocks['stock_id_array']);
+$taskFile = 'tdcc_task.json';
+file_put_contents($taskFile, json_encode($stocks, JSON_UNESCAPED_UNICODE));
+// $output = shell_exec('node ' . escapeshellarg(__DIR__ . '/prefetch_TDCC_batch.js') . ' ' . escapeshellarg($taskFile));
+// $results = json_decode($output, true);
+
 $end_time = microtime(true);
 $execution_time = round($end_time - $start_time, 2);
 echo "執行時間:{$execution_time} 秒\n";
-echo "檔數:{$count} 檔\n";
+
+echo file_get_contents($taskFile);
